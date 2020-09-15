40206dcb3ca7e3e983391120880cd4775b0c29a0
liuxindian <liuxindian@nemo-inc.com>
2020-09-15 12:53:05 +0800

    * fix accessibility service check

--- android/app/src/main/kotlin/com/demo/flutter_app2/MainActivity.kt
+++ android/app/src/main/kotlin/com/demo/flutter_app2/MainActivity.kt
@@ -5,16 +5,14 @@ import android.content.Intent
 import android.content.Context
 import android.os.Bundle
 import android.provider.Settings
-import android.text.TextUtils.SimpleStringSplitter
-import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.view.accessibility.AccessibilityManager
+import android.accessibilityservice.AccessibilityService
+import android.content.pm.ServiceInfo
+
 
 class MainActivity: FlutterActivity() {
-    companion object {
-        const val TAG = "MainActivity"
-
-        const val SERVICE_NAME = "MyAccessibilityService"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,46 +21,30 @@ class MainActivity: FlutterActivity() {
     }
 
     private fun showAlertDialog() {
-        if (!isAccessibilitySettingsOn(SERVICE_NAME, this)) {
+        if (!isAccessibilityServiceEnabled(this, MyAccessibilityService::class.java)) {
             val service = getString(R.string.service_name)
             val message = getString(R.string.notice_enable, service)
             AlertDialog.Builder(this)
-                        .setTitle(service)
-                        .setMessage(message)
-                        .setPositiveButton(android.R.string.ok) { _, _ ->
-                            openAccessibility()
-                        }
-                        .setNegativeButton(android.R.string.cancel) { _, _ ->
-
-                        }
-                        .create()
-                        .show()
+                    .setTitle(service)
+                    .setMessage(message)
+                    .setPositiveButton(android.R.string.ok) { dialog, _ ->
+                        openAccessibility()
+                        dialog.dismiss()
+                    }
+                    .setNegativeButton(android.R.string.cancel) { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .create()
+                    .show()
         }
     }
 
-    private fun isAccessibilitySettingsOn(accessibilityServiceName: String, context: Context): Boolean {
-        var accessibilityEnable = 0
-        val serviceName = context.packageName + "/" + accessibilityServiceName
-        try {
-            accessibilityEnable = Settings.Secure.getInt(context.contentResolver, Settings.Secure.ACCESSIBILITY_ENABLED, 0)
-        } catch (e: Exception) {
-            Log.e(TAG, "get accessibility enable failed, the err:" + e.message)
-        }
-        if (accessibilityEnable == 1) {
-            val stringColonSplitter = SimpleStringSplitter(':')
-            val settingValue: String? = Settings.Secure.getString(context.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
-            if (settingValue != null) {
-                stringColonSplitter.setString(settingValue)
-                while (stringColonSplitter.hasNext()) {
-                    val accessibilityService = stringColonSplitter.next()
-                    if (accessibilityService.equals(serviceName, ignoreCase = true)) {
-                        Log.v(TAG, "We've found the correct setting - accessibility is switched on!")
-                        return true
-                    }
-                }
-            }
-        } else {
-            Log.d(TAG, "Accessibility service disable")
+     private fun isAccessibilityServiceEnabled(context: Context, service: Class<out AccessibilityService?>): Boolean {
+        val am = context.getSystemService(ACCESSIBILITY_SERVICE) as AccessibilityManager
+        val enabledServices = am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK)
+        for (enabledService in enabledServices) {
+            val enabledServiceInfo: ServiceInfo = enabledService.resolveInfo.serviceInfo
+            if (enabledServiceInfo.packageName == context.packageName && enabledServiceInfo.name == service.name) return true
         }
         return false
     }
